57046e8bc3703ddc3439e1ba2b7f2775031ad760
Sebastian Ospina <juansebastian200103@gmail.com>
2022-04-23 18:37:48 -0400

    Se realizan las pruebas con el postman

--- FunctionProject/Fibonacci/index.js
+++ FunctionProject/Fibonacci/index.js
@@ -1,28 +1,24 @@
-var bigInt = require("big-integer");
+var memo = new Map();
+memo.set("1", BigInt(0));
+memo.set("2", BigInt(1));
+
 module.exports = async function (context, req) {
-
     context.log('JavaScript HTTP trigger function processed a request.');
-
     let nth = req.body.nth
-    let nth_1 = bigInt.one;
-    let nth_2 = bigInt.zero;
-    let answer = bigInt.zero;
-
     if (nth < 0)
         throw 'must be greater than 0'
-    else if (nth === 0)
-        answer = nth_2
-    else if (nth === 1)
-        answer = nth_1
-    else {
-        for (var i = 0; i < nth - 1; i++) {
-            answer = nth_2.add(nth_1)
-            nth_2 = nth_1
-            nth_1 = answer
-        }
+    function fibomemo(x) {
+        context.log(x)
+        return memo.has(x.toString()) ? memo.get(x.toString()) : fibo(x);
+    }
+
+    function fibo(nh) {
+        let res = fibomemo(nh - 1n) + fibomemo(nh - 2n);
+        memo.set(nh.toString(), res)
+        return res;
     }
     context.res = {
-        status: 202,
-        body: answer.toString()
+        body: fibomemo(BigInt(nth)).toString()
     };
 }
+
